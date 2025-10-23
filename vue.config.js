011386042af28ecaf0e923307e54d35e1d9f0efe
Yajin424 <2878992431@qq.com>
2025-10-23 16:18:48 +0800

    Update vue.config.js

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,6 @@
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' 
-    ? '/treasure-hunt-game.GitHub.io/' // 这里替换为你的GitHub仓库名称
+    ? '/treasure-hunt-game/' // 这里替换为你的GitHub仓库名称
     : '/'
+
 }
